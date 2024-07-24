e13a89099b26767b632472b9b1f9ef0c5d48f35d
TestDevelopeer <testdeveloper7967@gmail.com>
2024-07-24 16:42:37 +0300

    иконки +тайтл

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Maxton | Bootstrap 5 Admin Dashboard Template</title>
+    <title>ИСОиП | Абитуриент</title>
     <link href="/assets/css/all.min.css" rel="stylesheet" type="text/css">
     <!--favicon-->
     <link rel="icon" href="/assets/images/favicon-32x32.png" type="image/png">
